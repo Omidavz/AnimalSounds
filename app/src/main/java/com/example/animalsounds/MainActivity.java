a7d47a82f6808d3002cbba39aa77448ab5e0a0af
unknown <omidavz10@gmail.com>
2023-08-03 23:41:05 +0330

    first commit

---
+++ app/src/main/java/com/example/animalsounds/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.animalsounds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void playAnimalSound(View view){
+
+        Button clicked_btn = (Button) view ;
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this , getResources().getIdentifier(
+                clicked_btn.getTag().toString(),
+                "raw",
+                getPackageName()
+                )
+        );
+        mediaPlayer.start();
+
+
+    }
+}
